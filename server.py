42cab301d17435bcb9a1e299811cb98e50d12926
flexxlord <66664053+flexxlord@users.noreply.github.com>
2020-08-06 01:57:02 -0400

    adding all csv files, updating requirements.txt, and dash flask app

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, dash, dash_html_components as html
+import dash_core_components as dcc
+import plotly.express as px
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app=Flask(__name__)
 
